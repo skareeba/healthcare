dfe9877f1fc578e593ae9b75e35d59c13e2ccd9f
skareeba <miss.areebaanam@gmail.com>
2025-03-27 04:08:59 +0530

    fhfgn

--- data/INGESTION/cpt_codes.py
+++ data/INGESTION/cpt_codes.py
@@ -8,7 +8,7 @@ spark = SparkSession.builder \
 # configure variables
 BUCKET_NAME = "healthcare-bucket-26032025"
 CPT_BUCKET_PATH = f"gs://{BUCKET_NAME}/landing/cptcodes/*.csv"
-BQ_TABLE = "avd-databricks-demo.bronze_dataset.cpt_codes"
+BQ_TABLE = "aasgcpproject.bronze_dataset.cpt_codes"
 TEMP_GCS_BUCKET = f"{BUCKET_NAME}/temp/"
 
 # read from cpt
